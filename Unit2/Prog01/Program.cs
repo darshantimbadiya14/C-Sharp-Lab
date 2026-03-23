577fdfc0649dfe0e4694bc09d059a61c5e2f8a96
Darshan Timbadiya <darshan.timbadiya125553@marwadiuniversity.ac.in>
2026-03-23 18:33:18 +0530

    Initial Commit

--- Unit2/Prog01/Program.cs
+++ Unit2/Prog01/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+    Definittion 1
+    Write a program to print "Hello World!" on the console.
+*/
+
+using System;
+
+namespace Prog01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
